859110e7ca06d3c06466e40b34aa8cfce3086dea
Brahim Hamidi <bhamidi@student.42.fr>
2019-02-01 16:12:06 +0100

    add client setup conf

---
+++ src/client/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/app';
+
+ReactDOM.render(
+  <App name="client good setup" />,
+  document.getElementById('tetris')
+);
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+  	]
+  }
+};
+
