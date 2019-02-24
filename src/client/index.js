abf049bde87f1147bb43976b9024b9166d05d113
Brahim Hamidi <bhamidi@student.42.fr>
2019-02-24 20:08:43 +0100

    add redux dev tools + middleware

--- src/client/index.js
+++ src/client/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore, applyMiddleware} from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import ReduxThunk from 'redux-thunk'; 
 
 import App from './containers/app'
 import rootReducer from './reducers'
 
-const store = createStore(
-		rootReducer,
-		applyMiddleware(ReduxThunk)
-);
+ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+ const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(ReduxThunk)
+  ));
 
 ReactDOM.render(
 		<Provider store={store}>
