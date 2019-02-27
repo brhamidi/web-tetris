4c9acf33298df769cf9ae34ddc770e398baa1ec1
Brahim Hamidi <bhamidi@student.42.fr>
2019-02-27 14:43:13 +0100

    sort tabY

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,76 +1,76 @@
 import { List } from 'immutable';
 
 export const GameStatus = {
-		ERROR: 'ERROR',
-		RUNNING: 'RUNNING',
-		LOADING: 'LOADING',
-		WON: 'WON',
-		LOOSE: 'LOOSE',
-		BEGINNING: 'BEGINNING'
+	ERROR: 'ERROR',
+	RUNNING: 'RUNNING',
+	LOADING: 'LOADING',
+	WON: 'WON',
+	LOOSE: 'LOOSE',
+	BEGINNING: 'BEGINNING'
 };
 
 export const updateShape = (shape, posx, posy) => {
-		return Object.assign({}, shape, { pos: { x: posx + shape.pos.x, y: shape.pos.y + posy } })
+	return Object.assign({}, shape, { pos: { x: posx + shape.pos.x, y: shape.pos.y + posy } })
 }
 const updateSpectre = (spectre) => ({
-		type: 'UPDATE_SPECTRE',
-		spectre
+	type: 'UPDATE_SPECTRE',
+	spectre
 });
 
 const info = (info) => ({
-		type: 'INFO',
-		info
+	type: 'INFO',
+	info
 });
 
 export const setInfo = (socket, room, player) => {
-		return dispatch => {
-				socket.on('info_response', (msg => {
-						dispatch(info(msg));
-						dispatch(setStatusGame(GameStatus.BEGINNING));
-				}));
-				socket.emit('info', room, player);
-		}
+	return dispatch => {
+		socket.on('info_response', (msg => {
+			dispatch(info(msg));
+			dispatch(setStatusGame(GameStatus.BEGINNING));
+		}));
+		socket.emit('info', room, player);
+	}
 };
 
 export const setStatusGame = (status) => ({
-		type: 'STATUS_GAME',
-		status
+	type: 'STATUS_GAME',
+	status
 });
 
 const setCurrShape = (shape) => ({
-		type: 'SET_SHAPE',
-		shape
+	type: 'SET_SHAPE',
+	shape
 })
 
 const shapeRight = () => ({
-		type: 'SHAPE_RIGHT'
+	type: 'SHAPE_RIGHT'
 })
 
 const shapeLeft = () => ({
-		type: 'SHAPE_LEFT'
+	type: 'SHAPE_LEFT'
 })
 
 const shapeRotate = () => ({
-		type: 'SHAPE_ROTATE'
+	type: 'SHAPE_ROTATE'
 })
 
 const shapeDown = () => ({
-		type: 'SHAPE_DOWN'
+	type: 'SHAPE_DOWN'
 })
 
 const canDown = (shape, board) => {
-		const tabY = shape.shape.map(pos => pos.y);
-		const tab = tabY.filter(y => y + shape.pos.y >= 19);
+	const tabY = shape.shape.map(pos => pos.y);
+	const tab = tabY.filter(y => y + shape.pos.y >= 19);
 
-		if (tab.length === 0) {
-				const newValue = shape.shape.map(
-						e => ({x: shape.pos.x + e.x, y: shape.pos.y + 1 + e.y})
-				)
-				const colorTab = newValue.map(e => board.get(e.y).get(e.x))
+	if (tab.length === 0) {
+		const newValue = shape.shape.map(
+			e => ({x: shape.pos.x + e.x, y: shape.pos.y + 1 + e.y})
+		)
+		const colorTab = newValue.map(e => board.get(e.y).get(e.x))
 
-				return colorTab.filter(e => e !== undefined).length > 0 ?
-						false : true;
-		}
+		return colorTab.filter(e => e !== undefined).length > 0 ?
+			false : true;
+	}
 }
 
 const calculateMalus = (board, shape) => {
@@ -80,7 +80,7 @@ const calculateMalus = (board, shape) => {
 		else
 			return acc.push(curr.y + shape.pos.y);
 	}, List())
-		.map(e => ({ y: e, line: board.get(e)}) );
+	.map(e => ({ y: e, line: board.get(e)}) );
 
 	return lineY
 		.filter( ({ y, line }) => line.includes(undefined) === false)
@@ -110,139 +110,140 @@ const newTetrimino = (socket) => {
 }
 
 const mergeCurrShape = (shape) => ({
-		type: 'MERGE_SHAPE',
-		shape
+	type: 'MERGE_SHAPE',
+	shape
 })
 
 export const shapeShouldDown = (socket) => {
-		return (dispatch, getState) => {
-				const {currentShape, board} = getState();
+	return (dispatch, getState) => {
+		const {currentShape, board} = getState();
 
-				if (canDown(currentShape, board))
-						dispatch(shapeDown(board));
-				else {
-					dispatch(mergeCurrShape(currentShape));
-					return dispatch(newTetrimino(socket));
-				}
+		if (canDown(currentShape, board))
+			dispatch(shapeDown(board));
+		else {
+			dispatch(mergeCurrShape(currentShape));
+			return dispatch(newTetrimino(socket));
 		}
+	}
 }
 
 const startFall = (socket) => {
-		return dispatch => {
-				setInterval(() => dispatch(shapeShouldDown(socket)), 1000);
-				return dispatch(newTetrimino(socket));
-		}
+	return dispatch => {
+		setInterval(() => dispatch(shapeShouldDown(socket)), 1000);
+		return dispatch(newTetrimino(socket));
+	}
 }
 
 const canPut = (shape, board) => {
-		const shapeValue = shape.shape.map(({x, y}) => {
-				return {x: x + shape.pos.x, y: shape.pos.y + y};
-		})
+	const shapeValue = shape.shape.map(({x, y}) => {
+		return {x: x + shape.pos.x, y: shape.pos.y + y};
+	})
 
-		if (shapeValue.filter(({x, y}) => x < 0 || x > 9 || y < 0 || y > 19)
-				.length === 0)
-		{
-				const boardValue = shapeValue.map(({x, y}) => {
-						return board.get(y).get(x);
-				})
-				if (boardValue.filter(e => e !== undefined).length > 0)
-						return false;
-				return true;
-		}
-		return false;
+	if (shapeValue.filter(({x, y}) => x < 0 || x > 9 || y < 0 || y > 19)
+		.length === 0)
+	{
+		const boardValue = shapeValue.map(({x, y}) => {
+			return board.get(y).get(x);
+		})
+		if (boardValue.filter(e => e !== undefined).length > 0)
+			return false;
+		return true;
+	}
+	return false;
 }
 
 const OnPress = (socket) => {
-		return (dispatch, getState) => {
-				const handler = (event, getState) => {
-						const { currentShape, board } = getState();
-						const shape = currentShape;
+	return (dispatch, getState) => {
+		const handler = (event, getState) => {
+			const { currentShape, board } = getState();
+			const shape = currentShape;
 
-						if (event.code == "Space")
-								console.log('Dispatch chute shape');
-						if (event.code == "ArrowUp") {
-								const newShape = Object.assign(
-										{},
-										shape,
-										{
-												shape: shape.shape.map(e => ({
-														x: -e.y + shape.len - 1,
-														y: e.x
-												})) 
-										} )
-								if (canPut(newShape, board))
-										dispatch(shapeRotate());
-						}
-						if (event.code == "ArrowDown") {
-								event.preventDefault();
-								if (canPut(updateShape(shape, 0, 1), board))
-										dispatch(shapeDown());
-						}
-						if (event.code == "ArrowLeft")
-								if (canPut(updateShape(shape, -1, 0), board))
-										dispatch(shapeLeft());
-						if (event.code == "ArrowRight")
-								if (canPut(updateShape(shape, 1, 0), board))
-										dispatch(shapeRight());
-				}
-				window.addEventListener("keydown", (e) => handler(e, getState));
-				return dispatch(startFall(socket))
+			if (event.code == "Space")
+				console.log('Dispatch chute shape');
+			if (event.code == "ArrowUp") {
+				event.preventDefault();
+				const newShape = Object.assign(
+					{},
+					shape,
+					{
+						shape: shape.shape.map(e => ({
+							x: -e.y + shape.len - 1,
+							y: e.x
+						})) 
+					} )
+				if (canPut(newShape, board))
+					dispatch(shapeRotate());
+			}
+			if (event.code == "ArrowDown") {
+				event.preventDefault();
+				if (canPut(updateShape(shape, 0, 1), board))
+					dispatch(shapeDown());
+			}
+			if (event.code == "ArrowLeft")
+				if (canPut(updateShape(shape, -1, 0), board))
+					dispatch(shapeLeft());
+			if (event.code == "ArrowRight")
+				if (canPut(updateShape(shape, 1, 0), board))
+					dispatch(shapeRight());
 		}
+		window.addEventListener("keydown", (e) => handler(e, getState));
+		return dispatch(startFall(socket))
+	}
 }
 
 const addMalus = (n) => ({
-		type: 'MALUS',
-		n
+	type: 'MALUS',
+	n
 })
 
 const OnEvent = (socket) => {
-		return (dispatch, getState) => {
-				socket.on('spectre', (spectre) => {
-						dispatch(updateSpectre(spectre));
-				})
-				socket.on('tetrimino', (curr, next) => {
-						dispatch(setCurrShape(curr));
-				})
-				socket.on('won', () => {
-						console.log('i won');
-				})
-				socket.on('malus', (n) => {
-						dispatch(addMalus(n))
-				})
-				return dispatch(OnPress(socket));
-		}
+	return (dispatch, getState) => {
+		socket.on('spectre', (spectre) => {
+			dispatch(updateSpectre(spectre));
+		})
+		socket.on('tetrimino', (curr, next) => {
+			dispatch(setCurrShape(curr));
+		})
+		socket.on('won', () => {
+			console.log('i won');
+		})
+		socket.on('malus', (n) => {
+			dispatch(addMalus(n))
+		})
+		return dispatch(OnPress(socket));
+	}
 }
 
 export const OnCloseBoard = (socket) => {
-		return dispatch => {
-				socket.removeAllListeners("spectre");
-				socket.removeAllListeners("tetrimino");
-				socket.removeAllListeners("won");
-				socket.removeAllListeners("malus");
-		};
+	return dispatch => {
+		socket.removeAllListeners("spectre");
+		socket.removeAllListeners("tetrimino");
+		socket.removeAllListeners("won");
+		socket.removeAllListeners("malus");
+	};
 }
 
 export const OnStart = (socket) => {
-		return dispatch => {
-				socket.on('start', () => {
-						dispatch(setStatusGame(GameStatus.RUNNING));
-						socket.removeAllListeners("start");
-						return dispatch(OnEvent(socket))
-				});
-		};
+	return dispatch => {
+		socket.on('start', () => {
+			dispatch(setStatusGame(GameStatus.RUNNING));
+			socket.removeAllListeners("start");
+			return dispatch(OnEvent(socket))
+		});
+	};
 };
 
 export const startGame = (socket) => {
-		return dispatch => {
-				const press_enter = (event) => {
-						if (event.code == "Enter")
-						{
-								window.removeEventListener("keydown", press_enter);
-								socket.emit('start');
-								dispatch(setStatusGame(GameStatus.RUNNING));
-								return dispatch(OnEvent(socket))
-						}
-				}
-				window.addEventListener("keydown", press_enter);
+	return dispatch => {
+		const press_enter = (event) => {
+			if (event.code == "Enter")
+			{
+				window.removeEventListener("keydown", press_enter);
+				socket.emit('start');
+				dispatch(setStatusGame(GameStatus.RUNNING));
+				return dispatch(OnEvent(socket))
+			}
 		}
+		window.addEventListener("keydown", press_enter);
+	}
 }
--- src/client/reducers/board.js
+++ src/client/reducers/board.js
@@ -1,33 +1,38 @@
 import { List } from 'immutable';
 
 const emptyBoard =
-		List().set(19, undefined).map(e => List().set(9, undefined));
+	List().set(19, undefined).map(e => List().set(9, undefined));
 
 const board = (board = emptyBoard, action) => {
-		switch (action.type) {
-				case 'MALUS':
-						const f = (board, n) => {
-								if (n === 0)
-									return board;
-								return f(board.delete(0).insert(19, List().set(9, 0).map(e => 'black')), n-1);
-						}
-						return f(board, action.n);
-				case 'DESTROY':
-						const newTab = action.tabY.reduce((acc, curr) => {
-								return acc.delete(curr).insert(0, List().set(9, undefined));
-						}, board);
-						return newTab;
-				case 'MERGE_SHAPE':
-						return (
-								action.shape.shape.reduce((acc, curr) => {
-										const y = curr.y + action.shape.pos.y;
-										const x = curr.x + action.shape.pos.x;
-										return acc.setIn([y, x], action.shape.color);
-								}, board)
-						);
-				default:
-						return board;
-		}
+	switch (action.type) {
+		case 'MALUS':
+			const f = (board, n) => {
+				if (n === 0)
+					return board;
+				return f(board.delete(0).insert(19, List().set(9, 0).map(e => 'black')), n-1);
+			}
+			return f(board, action.n);
+		case 'DESTROY':
+			const newTab = action.tabY.sort((a, b) => {
+				if (a < b) { return -1; }
+				if (a > b) { return 1; }
+				if (a === b) { return 0; }
+			});
+
+			return newTab.reduce((acc, curr) => {
+				return acc.delete(curr).insert(0, List().set(9, undefined));
+			}, board);
+		case 'MERGE_SHAPE':
+			return (
+				action.shape.shape.reduce((acc, curr) => {
+					const y = curr.y + action.shape.pos.y;
+					const x = curr.x + action.shape.pos.x;
+					return acc.setIn([y, x], action.shape.color);
+				}, board)
+			);
+		default:
+			return board;
+	}
 };
 
 export default board;
