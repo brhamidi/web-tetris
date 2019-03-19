9355f982192f90c4f42c573b5c24be9e1d049351
Matthias Srun <matthias.srun@gmail.com>
2019-03-19 18:02:20 +0100

    add untested file

--- __test__/Score.spec.js
+++ __test__/Score.spec.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Score from '../src/client/components/score';
 
+import AppContainer from '../src/client/containers/AppContainer';
+import SpectreContainer from '../src/client/containers/SpectreContainer';
+import BoardContainer from '../src/client/containers/BoardContainer';
+import NextShapeContainer from '../src/client/containers/NextShapeContainer';
+import ScoreContainer from '../src/client/containers/ScoreContainer';
+
 describe('Score', () => {
 	test('renders correctly', () => {
 		const wrapper = shallow(<Score />);
